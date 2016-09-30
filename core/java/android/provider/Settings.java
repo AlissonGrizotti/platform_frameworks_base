e8cc98ed6f357d8354a2ac1c5cf09fb5c8b9fc3d
XXMrHyde <xxmrhyde@gmx.co.uk>
2016-09-30 07:22:19 -0400

    SystemUI: Visualizer customizations
    mydongistiny: Fixed up for pie and cleaned up Visualizer
    Change-Id: Ife08e2de82ce1598b3541b18721308d75ed148aa
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5094,6 +5094,18 @@ public final class Settings {
         public static final String SCREEN_STATE_ON_DELAY = "screen_state_on_delay";
 
         /**
+         *  Weather to use a custom color for the visualizer on the lock screen
+         * @hide
+         */
+        public static final String LOCK_SCREEN_VISUALIZER_USE_CUSTOM_COLOR = "lock_screen_visualizer_use_custom_color";
+
+        /**
+         *  Custom color of the visualizer on the lock screen
+         * @hide
+         */
+        public static final String LOCK_SCREEN_VISUALIZER_CUSTOM_COLOR = "lock_screen_visualizer_custom_color";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
