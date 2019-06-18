5f7ce35c753d052bc6ebe52bf4e2e99dfeb4e774
rajkale99 <kaleraj.rk@gmail.com>
2019-06-18 16:39:55 +0000

    Fix Home navigation weired touch issue

--- packages/SystemUI/src/com/android/systemui/statusbar/NotificationMediaManager.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/NotificationMediaManager.java
@@ -25,7 +25,6 @@ import android.media.session.PlaybackState;
 import android.os.UserHandle;
 import android.util.Log;
 
-import com.android.systemui.statusbar.phone.StatusBar;
 import com.android.systemui.Dumpable;
 import com.android.systemui.SysUiServiceProvider;
 
